c9a32cbff5776a9a06ce70947bdc27b2861f3c16
Yuval Maman <yuvilio@yahoo.com>
2015-01-10 14:57:41 -0500

    remove dupe file

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,9 @@ var deployGh = require('gulp-gh-pages');
 
 gulp.task('deploy-gh-pages', function () {
 
-  var options = {};
-  return gulp.src('./dist/doc/**/*')
-  .pipe(deployGh(options));
-});
-
-//deploy docs to Github Pages
-gulp.task('deploy-gh-pages',  function() {
-
-  console.log('deploy-gh-pages');
-
+  // var options = {
+  //   remoteUrl: '/'
+  // };
+  return gulp.src('./dist/**/*')
+  .pipe(deployGh());
 });
